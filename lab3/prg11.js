354e60d8f6140b9618739377aec0120b93927df4
Ayush Singh <singhneetu7687@gmail.com>
2026-09-03 14:31:48 +0530

    add

--- lab3/prg11.js
+++ lab3/prg11.js
@@ -2,14 +2,34 @@ import http from "http";
 
 const server = http.createServer((req, res) => {
   if (req.url === "/" && req.method === "GET") res.end("Home Page");
-  else if (req.url === "/product" && req.method === "GET")
-    res.end("Show Product");
-  else if (req.url === "/product" && req.method === "POST")
-    req.end("add product");
-  else if (req.url === "/product" && req.method === "PUT")
-    req.end("Update product");
-  else if (req.url === "/product" && req.method === "DELETE")
-    req.end("Remove Product");
+  else if (req.url === "/product" && req.method === "GET"){
+    const products=[
+      {
+        id:1,
+        name:"Mobile",
+        price:10000,
+      },
+      {
+        id:2,
+        name:"Duster",
+        price:100,
+      }
+    ];
+    res.end(JSON.stringify(products));     // res.end() send only strings
+  }
+  else if (req.url === "/product" && req.method === "POST"){
+    res.end("Add Product");
+  }
+  else if (req.url === "/product" && req.method === "PUT"){
+    res.end("Update Product");
+  }
+  else if (req.url === "/product" && req.method === "DELETE"){
+    res.end("Remove Product");
+  }
+else{
+    res.statusCode = 404;
+    res.end("Not Found");
+}
 });
 
 server.listen(3007, () => console.log("prg11 is running"));
